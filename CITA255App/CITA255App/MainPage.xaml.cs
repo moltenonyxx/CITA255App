8691096d1c360e8af1b4f42b4ac745eae666f8ad
moltenonyxx <everettrivenburg@gmail.com>
2026-09-03 18:00:35 -0400

    Finished button in class and 1st HW assignment

--- CITA255App/CITA255App/MainPage.xaml.cs
+++ CITA255App/CITA255App/MainPage.xaml.cs
@@ -6,6 +6,23 @@
         public MainPage()
         {
             InitializeComponent();
+
+        }
+
+        private void onButtonClicked(object sender, EventArgs e)
+        {
+            string entrytext = compBox.Text;
+            nameText.Text = $"Is your complaint:{entrytext}?";
+           
+        }
+
+        private void cels(object sender, EventArgs e)
+        {
+            float celsEntry = float.Parse(CelsBox.Text);
+            float Celsius = celsEntry * 5.0f / 9.0f;
+            resultLabel.Text = $"Your temp in Celsius is:{celsEntry} °";
+
+
         }
     }
 }
