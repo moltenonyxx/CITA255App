e9653c76ec9c6bfea808a00e1305470524da9443
moltenonyxx <everettrivenburg@gmail.com>
2026-09-08 16:40:00 -0400

    added fahrenhiet conversion kinda

--- CITA255App/CITA255App/MainPage.xaml.cs
+++ CITA255App/CITA255App/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CITA255App
+﻿using System.Numerics;
+
+namespace CITA255App
 {
     public partial class MainPage : ContentPage
     {
@@ -8,13 +10,13 @@
             InitializeComponent();
 
         }
-
         private void onButtonClicked(object sender, EventArgs e)
         {
             string entrytext = compBox.Text;
             nameText.Text = $"Is your complaint:{entrytext}?";
-           
+
         }
+
 
         private void cels(object sender, EventArgs e)
         {
@@ -23,6 +25,12 @@
             resultLabel.Text = $"Your temp in Celsius is: {Celcius :N1}°";
 
 
+        }
+        private void far(object sender, EventArgs e)
+        {
+            float farEntry = float.Parse(farBox.Text);
+            float fahrenheit = (farEntry * 6.0f / 9.0f) + 32;
+            resultLabelfar.Text = $"Your temp in fahrenheit is: {fahrenheit:N1}°";
         }
     }
 }
